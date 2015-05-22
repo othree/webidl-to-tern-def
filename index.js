66571defc4af3b15c53ed6e9465de43f0d0a4936
othree <othree@gmail.com>
2015-05-22 18:44:59 +0800

    Loader

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var loader = require('./loader.js');
+// var transformer = require('./transformer.js');
+// var generator = require('./generator.js');
+
+var walk = require('walk');
+var files = [];
+
+var st = {};
+
+// Walker options
+var walker  = walk.walk('./webidl/webidl', { followLinks: false });
+
+walker.on('file', function(root, stat, next) {
+  // Add this file to the list of files
+  st = loader.file(root + '/' + stat.name, st);
+  next();
+});
+
+walker.on('end', function() {
+  "use strict";
+
+  // var data = transformer.run(st);
+  // var code = generator.run(data);
+});
+
